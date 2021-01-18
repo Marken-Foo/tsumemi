a9b2f2e72a4267d5722f4ed4f8093a82e56b5db9
Marken Foo <41288521+Marken-Foo@users.noreply.github.com>
2021-01-18 22:43:41 +0800

    Refactor Model into own file

--- kif_browser_gui.py
+++ kif_browser_gui.py
@@ -1,12 +1,10 @@
 import os
-import re
 import tkinter as tk
 
-from enum import Enum
 from tkinter import filedialog, messagebox, ttk
 
-import kif_parser
 from board_canvas import BoardCanvas
+from model import Model
 from split_timer import SplitTimer
 
 
@@ -229,111 +227,6 @@ class ProblemListPane(ttk.Frame):
         # Set time column for item at given index
         item = self.tvw.get_children()[idx]
         self.tvw.set(item, column="time", value=time_str)
-        return
-
-
-class ProblemStatus(Enum):
-    NONE = 0; CORRECT = 1; WRONG = 2; SKIP = 3
-
-
-class Problem:
-    '''Data class representing one tsume problem.'''
-    def __init__(self, filename):
-        self.filename = filename
-        self.time = None
-        self.status = ProblemStatus.NONE
-        return
-
-
-class Model:
-    # To follow MVC principles, we refactor the business logic of MainWindow out here.
-    problems = []
-    curr_prob_idx = None
-    curr_prob = None
-    
-    directory = None
-    
-    reader = kif_parser.KifReader()
-    solution = ""
-    
-    @staticmethod
-    def natural_sort_key(str, _nsre=re.compile(r'(\d+)')):
-        return [int(c) if c.isdigit() else c.lower() for c in _nsre.split(str)]
-    
-    def __init__(self):
-        return
-    
-    def read_problem(self):
-        # Read current problem into reader.
-        # Try any likely encodings for the KIF files
-        encodings = ["cp932", "utf-8"]
-        for e in encodings:
-            try:
-                with open(self.curr_prob.filename, "r", encoding=e) as kif:
-                    self.reader.parse_kif(kif)
-                    self.solution = "　".join(self.reader.moves)
-            except UnicodeDecodeError:
-                pass
-            else:
-                break
-        return self.reader
-    
-    def set_directory(self, directory):
-        self.directory = directory
-        with os.scandir(directory) as it:
-            self.problems = [
-                Problem(os.path.join(directory, entry.name))
-                for entry in it
-                if entry.name.endswith(".kif") or entry.name.endswith(".kifu")
-            ]
-        self.problems.sort(key=lambda p: Model.natural_sort_key(p.filename))
-        self.curr_prob_idx = 0
-        self.curr_prob = self.problems[self.curr_prob_idx]
-        self.read_problem()
-        return
-    
-    def open_next_file(self):
-        if self.curr_prob_idx+1 >= len(self.problems):
-            return False
-        self.curr_prob = self.problems[self.curr_prob_idx + 1]
-        self.curr_prob_idx += 1
-        self.read_problem()
-        return True
-    
-    def open_prev_file(self):
-        if self.curr_prob_idx-1 < 0:
-            return False
-        self.curr_prob = self.problems[self.curr_prob_idx - 1]
-        self.curr_prob_idx -= 1
-        self.read_problem()
-        return True
-    
-    def open_file(self, idx):
-        if idx >= len(self.problems) or idx < 0:
-            return False
-        self.curr_prob = self.problems[idx]
-        self.curr_prob_idx = idx
-        self.read_problem()
-        return True
-    
-    def set_correct(self):
-        if self.curr_prob is not None:
-            self.curr_prob.status = ProblemStatus.CORRECT
-        return
-    
-    def set_wrong(self):
-        if self.curr_prob is not None:
-            self.curr_prob.status = ProblemStatus.WRONG
-        return
-    
-    def set_skip(self):
-        if self.curr_prob is not None:
-            self.curr_prob.status = ProblemStatus.SKIP
-        return
-    
-    def set_time(self, time):
-        if self.curr_prob is not None:
-            self.curr_prob.time = time
         return
 
 
