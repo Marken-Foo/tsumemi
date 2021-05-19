affdc49a14ab33c5cc18eb2d866d927b77fc92c6
Marken Foo <41288521+Marken-Foo@users.noreply.github.com>
2021-05-19 16:57:10 +0800

    Add type hints for event.py

--- tsumemi/src/tsumemi/event.py
+++ tsumemi/src/tsumemi/event.py
@@ -1,18 +1,27 @@
+from __future__ import annotations
+
 from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Any, Callable, Dict, List, Type
 
 
 class Event:
     """Base class for Events of all kinds.
     """
-    def __init__(self):
-        pass
+    def __init__(self) -> None:
+        return
 
 
 class IObserver(ABC):
     """Base class for any class that needs to observe events.
     """
+    def __init__(self) -> None:
+        self.NOTIFY_ACTIONS: Dict[Type[Event], Callable[..., Any]] = {}
+    
     @abstractmethod
-    def on_notify(event):
+    def on_notify(self, event: Event) -> None:
         event_type = type(event)
         if event_type in self.NOTIFY_ACTIONS:
             self.NOTIFY_ACTIONS[event_type](event)
@@ -22,11 +31,15 @@ class IObserver(ABC):
 class Emitter():
     """Base class for any class that needs to emit events.
     """
-    def add_observer(self, observer):
+    def __init__(self) -> None:
+        self.observers: List[IObserver] = []
+        return
+    
+    def add_observer(self, observer: IObserver) -> None:
         self.observers.append(observer)
         return
     
-    def remove_observer(self, observer):
+    def remove_observer(self, observer: IObserver) -> None:
         try:
             self.observers.remove(observer)
         except ValueError:
@@ -34,7 +47,7 @@ class Emitter():
             pass
         return
     
-    def _notify_observers(self, event):
+    def _notify_observers(self, event: Event) -> None:
         for observer in self.observers:
             observer.on_notify(event)
         return
