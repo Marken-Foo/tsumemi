236466d089c607316e68a7e2124baedc9f173280
Marken Foo <41288521+Marken-Foo@users.noreply.github.com>
2022-08-09 19:05:32 +0800

    Split Menubar and StatisticsDialog classes from kif_browser_gui.py

--- tsumemi/src/tsumemi/kif_browser_gui.py
+++ tsumemi/src/tsumemi/kif_browser_gui.py
@@ -1,12 +1,11 @@
 from __future__ import annotations
 
 import datetime
-import functools
 import logging.config
 import os
 import tkinter as tk
 
-from tkinter import filedialog, messagebox, ttk
+from tkinter import filedialog, ttk
 from typing import TYPE_CHECKING
 
 import tsumemi.src.shogi.parsing.kif as kif
@@ -22,6 +21,8 @@ import tsumemi.src.tsumemi.timer as timer
 import tsumemi.src.tsumemi.timer_controller as timecon
 
 from tsumemi.src.tsumemi.main_window_view import MainWindowView
+from tsumemi.src.tsumemi.menubar import Menubar
+from tsumemi.src.tsumemi.statistics_window import StatisticsDialog
 
 if TYPE_CHECKING:
     from typing import Callable, Optional
@@ -265,155 +266,6 @@ class Bindings:
     def unbind_shortcuts(target, shortcuts):
         for keypress in shortcuts.keys():
             target.unbind(keypress)
-        return
-
-
-class Menubar(tk.Menu):
-    """GUI class for the menubar at the top of the main window.
-    """
-    def __init__(self,
-            parent: tk.Tk,
-            controller: RootController,
-            *args, **kwargs
-        ) -> None:
-        self.controller = controller
-        super().__init__(parent, *args, **kwargs)
-
-        # Set cascades
-        menu_file = tk.Menu(self)
-        self.add_cascade(menu=menu_file, label="File")
-        menu_solving = tk.Menu(self)
-        self.add_cascade(menu=menu_solving, label="Solving")
-        menu_settings = tk.Menu(self)
-        self.add_cascade(menu=menu_settings, label="Settings")
-
-        # File
-        menu_file.add_command(
-            label="Open folder...",
-            command=self.controller.open_folder,
-            accelerator="Ctrl+O",
-            underline=0,
-        )
-        menu_file.add_command(
-            label="Open all subfolders...",
-            command=self.controller.open_folder_recursive,
-            accelerator="Ctrl+Shift+O",
-        )
-        menu_file.add_separator()
-        menu_file.add_command(
-            label="Copy SFEN of current position",
-            command=self.controller.copy_sfen_to_clipboard,
-        )
-        # Solving
-        menu_solving.add_command(
-            label="Get statistics",
-            command=self.controller.generate_statistics,
-        )
-        menu_solving.add_command(
-            label="Export results as CSV...",
-            command=self.controller.export_prob_list_csv,
-        )
-        menu_solving.add_separator()
-        menu_solving.add_command(
-            label="Clear solving statuses",
-            command=self.controller.clear_statuses,
-        )
-        menu_solving.add_command(
-            label="Clear solving times",
-            command=self.controller.clear_times,
-        )
-        menu_solving.add_command(
-            label="Clear results",
-            command=self.controller.clear_results,
-        )
-        # Settings
-        menu_settings.add_command(
-            label="Settings...",
-            command=self.controller.open_settings_window
-            # command=lambda: SettingsWindow(controller=self.controller.settings),
-        )
-        menu_settings.add_command(
-            label="About tsumemi",
-            command=functools.partial(
-                messagebox.showinfo,
-                title="About tsumemi",
-                message="Written in Python 3 by Marken Foo. For the shogi community. KIF files sold separately."
-            )
-        )
-        # Bind to main window
-        parent["menu"] = self
-        return
-
-
-class StatisticsDialog(tk.Toplevel):
-    def __init__(self, stats: plistcon.ProblemListStats,
-            *args, **kwargs
-        ) -> None:
-        """Dialog box generating and displaying solving statistics.
-        """
-        super().__init__(*args, **kwargs)
-        num_folder = stats.get_num_total()
-        num_correct = stats.get_num_correct()
-        num_wrong = stats.get_num_wrong()
-        num_skip = stats.get_num_skip()
-        num_seen = num_correct + num_wrong + num_skip
-        total_time = stats.get_total_time()
-        avg_time = (timer.Time(total_time.seconds / num_seen)
-            if num_seen != 0 else timer.Time(0)
-        )
-        date_time_now = datetime.datetime.now()
-        message_strings = [
-            f"Folder name: {stats.directory}",
-            f"Report generated: {date_time_now.strftime('%Y-%m-%d %H:%M:%S')}",
-            "",
-            f"Total time: {total_time.to_hms_str(places=1)}",
-            f"Problems in folder: {num_folder}",
-            f"Problems seen: {num_seen}",
-            f"Problems correct: {num_correct}",
-            f"Problems wrong: {num_wrong}",
-            f"Problems skipped: {num_skip}",
-            f"Average time per problem: {avg_time.to_hms_str(places=1)}",
-        ]
-        slowest_prob = stats.get_slowest_problem()
-        if slowest_prob is not None:
-            _slowest_filename = os.path.basename(slowest_prob.filepath)
-            _slowest_time = slowest_prob.time
-            assert _slowest_time is not None
-            message_strings.append(
-                f"Longest time taken: {_slowest_time.to_hms_str(places=1)} ({_slowest_filename})"
-            )
-        fastest_prob = stats.get_fastest_problem()
-        if fastest_prob is not None:
-            _fastest_filename = os.path.basename(fastest_prob.filepath)
-            _fastest_time = fastest_prob.time
-            assert _fastest_time is not None
-            message_strings.append(
-                f"Shortest time taken: {_fastest_time.to_hms_str(places=1)} ({_fastest_filename})"
-            )
-        report_text = "\n".join(message_strings)
-
-        self.title("Solving statistics")
-        lbl_report = ttk.Label(self, text=report_text)
-        lbl_message = ttk.Label(self, text="Copy your results from below:")
-        txt_report = tk.Text(self, width=30, height=3, wrap="none")
-        txt_report.insert(tk.INSERT, report_text)
-        txt_report.config(state="disabled")
-        vsc_txt_report = ttk.Scrollbar(self, orient="vertical", command=txt_report.yview)
-        txt_report["yscrollcommand"] = vsc_txt_report.set
-        hsc_txt_report = ttk.Scrollbar(self, orient="horizontal", command=txt_report.xview)
-        txt_report["xscrollcommand"] = hsc_txt_report.set
-        btn_ok = ttk.Button(self, text="OK", command=self.destroy)
-
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(2, weight=1)
-        for child in self.winfo_children():
-            child.grid_configure(padx=5, pady=5)
-        lbl_report.grid(row=0, column=0, columnspan=2)
-        lbl_message.grid(row=1, column=0, columnspan=2)
-        txt_report.grid(row=2, column=0, sticky="NSEW")
-        vsc_txt_report.grid(row=2, column=1, sticky="NS")
-        hsc_txt_report.grid(row=3, column=0, sticky="EW")
-        btn_ok.grid(row=4, column=0)
         return
 
 
