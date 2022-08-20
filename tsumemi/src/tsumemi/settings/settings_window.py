763c153ccf9cf6424029d6e3cd7b78a4acc161d0
Marken Foo <41288521+Marken-Foo@users.noreply.github.com>
2022-08-21 00:15:48 +0800

    Remove trailing whitespace from settings_window.py

--- tsumemi/src/tsumemi/settings/settings_window.py
+++ tsumemi/src/tsumemi/settings/settings_window.py
@@ -16,7 +16,7 @@ if TYPE_CHECKING:
 class OptionsFrame(ttk.Frame):
     def __init__(self, parent: SettingsWindow) -> None:
         super().__init__(parent)
-        
+
         self.frm_board_options = ttk.LabelFrame(self, text="Board appearance")
         self.frm_board_options.grid(row=0, column=0, sticky="EW")
         self.frm_board_skin = BoardSkinSelectionFrame(
@@ -34,11 +34,11 @@ class OptionsFrame(ttk.Frame):
         self.frm_board_skin.grid(row=0, column=0, sticky="EW")
         self.frm_komadai_skin.grid(row=1, column=0, sticky="EW")
         self.frm_piece_skin.grid(row=2, column=0, sticky="EW")
-        
+
         self.frm_board_skin.grid_columnconfigure(0, weight=1)
         self.frm_komadai_skin.grid_columnconfigure(0, weight=1)
         self.frm_piece_skin.grid_columnconfigure(0, weight=1)
-        
+
         self.frm_notation_options = ttk.LabelFrame(self, text="Notation")
         self.frm_notation_options.grid(row=1, column=0, sticky="EW")
         self.frm_notation_choice = NotationSelectionFrame(
@@ -54,12 +54,12 @@ class SettingsWindow(tk.Toplevel):
     def __init__(self, controller: Settings, *args, **kwargs) -> None:
         self.controller = controller
         super().__init__(*args, **kwargs)
-        
+
         self.title("Settings")
-        
+
         self.options_frame = OptionsFrame(self)
         self.options_frame.grid(row=0, column=0)
-        
+
         buttons_frame = ttk.Frame(self)
         buttons_frame.grid(row=1, column=0, sticky="EW")
         btn_okay = ttk.Button(
@@ -69,7 +69,7 @@ class SettingsWindow(tk.Toplevel):
         btn_apply = ttk.Button(buttons_frame, text="Apply", command=self.save)
         btn_apply.grid(row=0, column=1)
         return
-    
+
     def save(self) -> None:
         self.controller.update_board_skin_settings()
         self.controller.update_komadai_skin_settings()
@@ -78,7 +78,7 @@ class SettingsWindow(tk.Toplevel):
         self.controller.write_current_settings_to_file()
         self.controller.push_settings_to_controller()
         return
-    
+
     def save_and_quit(self) -> None:
         self.save()
         self.destroy()
