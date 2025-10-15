4420d07fa8e89491d0a2a67af0f50f68bb0f2ca8
Marken Foo <41288521+Marken-Foo@users.noreply.github.com>
2025-10-16 01:54:16 +0800

    Explicit specify side strategy for koma_test

--- tsumemi/src/shogi/tests/koma_test.py
+++ tsumemi/src/shogi/tests/koma_test.py
@@ -3,33 +3,37 @@ from hypothesis import assume, given, strategies as st
 from tsumemi.src.shogi.basetypes import Koma, KomaType, Side
 
 
+def valid_side() -> st.SearchStrategy:
+    return st.sampled_from([Side.SENTE, Side.GOTE, Side.UWATE, Side.SHITATE])
+
+
 @st.composite
 def valid_koma(draw: st.DrawFn) -> Koma:
-    side = draw(st.sampled_from(Side))
+    side = draw(valid_side())
     ktype = draw(st.sampled_from(KomaType))
     assume(ktype != KomaType.NONE and ktype != KomaType(13))
     return Koma.make(side, ktype)
 
 
-@given(st.sampled_from(Side), st.sampled_from(KomaType))
+@given(valid_side(), st.sampled_from(KomaType))
 def test_get_side(side: Side, ktype: KomaType):
     koma = Koma.make(side, ktype)
     assert koma.side() == side
 
 
-@given(st.sampled_from(Side), st.sampled_from(KomaType))
+@given(valid_side(), st.sampled_from(KomaType))
 def test_get_komatype(side: Side, ktype: KomaType):
     koma = Koma.make(side, ktype)
     assert KomaType.get(koma) == ktype
 
 
-@given(st.sampled_from(Side), st.sampled_from(KomaType))
+@given(valid_side(), st.sampled_from(KomaType))
 def test_promote(side: Side, ktype: KomaType):
     koma = Koma.make(side, ktype)
     assert koma.promote() == Koma.make(side, ktype.promote())
 
 
-@given(st.sampled_from(Side), st.sampled_from(KomaType))
+@given(valid_side(), st.sampled_from(KomaType))
 def test_is_promoted(side: Side, ktype: KomaType):
     koma = Koma.make(side, ktype)
     assert koma.is_promoted() == ktype.is_promoted()
