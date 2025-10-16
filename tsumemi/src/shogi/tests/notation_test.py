1e9b5961da84963011684d9cde6efc4bc56a0fbb
Marken Foo <41288521+Marken-Foo@users.noreply.github.com>
2025-10-17 00:33:32 +0800

    Add tests for Japanese drops

--- tsumemi/src/shogi/tests/notation_test.py
+++ tsumemi/src/shogi/tests/notation_test.py
@@ -3,13 +3,16 @@ import itertools
 
 import pytest
 
+from hypothesis import given, strategies as st
 from typing import TYPE_CHECKING
 
-from tsumemi.src.shogi.basetypes import KOMA_FROM_SFEN
+from tsumemi.src.shogi.basetypes import KOMA_FROM_SFEN, KomaType, Side
 from tsumemi.src.shogi.notation import WesternMoveWriter
 from tsumemi.src.shogi.notation.move_writers import JapaneseMoveWriter
 from tsumemi.src.shogi.position import Position
 from tsumemi.src.shogi.square import Square
+from tsumemi.src.shogi.tests.position_hand_test import TEST_HAND_KTYPES
+from tsumemi.src.shogi.tests.square_test import board_squares
 
 if TYPE_CHECKING:
     from collections.abc import Sequence
@@ -185,3 +188,30 @@ def test_japanese_notation(
         pos.set_koma(koma, square)
     move = pos.create_move(start_sq=start_sq, end_sq=end_sq, is_promotion=is_promotion)
     assert move_writer.write_move(move, pos) == expected
+
+
+kanji_from_hand_ktype = {
+    KomaType.FU: "歩",
+    KomaType.KY: "香",
+    KomaType.KE: "桂",
+    KomaType.GI: "銀",
+    KomaType.KI: "金",
+    KomaType.KA: "角",
+    KomaType.HI: "飛",
+}
+
+
+@given(
+    board_squares(),
+    st.sampled_from(TEST_HAND_KTYPES),
+    st.sampled_from([Side.SENTE, Side.GOTE]),
+)
+def test_japanese_drop(sq: Square, ktype: KomaType, side: Side):
+    move_writer = JapaneseMoveWriter()
+    pos = Position()
+    pos.set_hand_koma_count(side, ktype, 1)
+    move = pos.create_drop_move(side, ktype, sq)
+    assert (
+        move_writer.write_move(move, pos)
+        == f"{sq.to_japanese()}{kanji_from_hand_ktype[ktype]}打"
+    )
--- tsumemi/src/shogi/tests/square_test.py
+++ tsumemi/src/shogi/tests/square_test.py
@@ -4,6 +4,32 @@ from tsumemi.src.shogi.basetypes import Side
 from tsumemi.src.shogi.square import Square
 
 
+FULL_WIDTH_NUMBERS = {
+    0: "０",
+    1: "１",
+    2: "２",
+    3: "３",
+    4: "４",
+    5: "５",
+    6: "６",
+    7: "７",
+    8: "８",
+    9: "９",
+}
+
+KANJI_NUMBERS = {
+    1: "一",
+    2: "二",
+    3: "三",
+    4: "四",
+    5: "五",
+    6: "六",
+    7: "七",
+    8: "八",
+    9: "九",
+}
+
+
 @st.composite
 def board_squares(draw: st.DrawFn) -> Square:
     row_num = draw(st.integers(1, 9))
@@ -41,3 +67,9 @@ def test_is_same_row(sq1: Square, sq2: Square):
     (_, row1) = sq1.get_cr()
     (_, row2) = sq2.get_cr()
     assert sq1.is_same_row(sq2) == sq2.is_same_row(sq1) == (row1 == row2)
+
+
+@given(board_squares())
+def test_to_japanese(sq: Square):
+    (col, row) = sq.get_cr()
+    assert sq.to_japanese() == f"{FULL_WIDTH_NUMBERS[col]}{KANJI_NUMBERS[row]}"
