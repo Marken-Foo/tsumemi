dabbf23d9c5e4a2141b3020eb694e8332e1335fd
Marken Foo <41288521+Marken-Foo@users.noreply.github.com>
2025-10-18 14:40:15 +0800

    Add western notation drop tests

--- tsumemi/src/shogi/tests/notation_western_test.py
+++ tsumemi/src/shogi/tests/notation_western_test.py
@@ -1,18 +1,22 @@
 from __future__ import annotations
 
+from hypothesis import given, strategies as st
 import pytest
 
 from typing import TYPE_CHECKING
 
+from tsumemi.src.shogi.basetypes import Koma, SFEN_FROM_KOMA, Side
 from tsumemi.src.shogi.notation import WesternMoveWriter
 from tsumemi.src.shogi.position import Position
 from tsumemi.src.shogi.tests.notation_test import (
     get_parametrization_from_test_cases,
     read_notation_test_file,
 )
+from tsumemi.src.shogi.tests.position_hand_test import TEST_HAND_KTYPES
+from tsumemi.src.shogi.tests.square_test import board_squares
 
 if TYPE_CHECKING:
-    from tsumemi.src.shogi.basetypes import Koma
+    from tsumemi.src.shogi.basetypes import KomaType
     from tsumemi.src.shogi.square import Square
 
 
@@ -43,3 +47,20 @@ def test_western_notation(
         pos.set_koma(koma, square)
     move = pos.create_move(start_sq=start_sq, end_sq=end_sq, is_promotion=is_promotion)
     assert move_writer.write_move(move, pos) == expected
+
+
+@given(
+    board_squares(),
+    st.sampled_from(TEST_HAND_KTYPES),
+    st.sampled_from([Side.SENTE, Side.GOTE]),
+)
+def test_western_drop(sq: Square, ktype: KomaType, side: Side):
+    move_writer = WesternMoveWriter()
+    pos = Position()
+    pos.set_hand_koma_count(side, ktype, 1)
+    move = pos.create_drop_move(side, ktype, sq)
+    col, row = sq.get_cr()
+    assert (
+        move_writer.write_move(move, pos)
+        == f"{SFEN_FROM_KOMA[Koma.make(side, ktype)].upper()}*{col}{row}"
+    )
