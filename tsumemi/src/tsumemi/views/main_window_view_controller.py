f7352023753271218ff4c53917662aa0ff0f1dae
Marken Foo <41288521+Marken-Foo@users.noreply.github.com>
2025-10-20 22:39:08 +0800

    Clean main_window_view_controller.py

--- tsumemi/src/tsumemi/views/main_window_view_controller.py
+++ tsumemi/src/tsumemi/views/main_window_view_controller.py
@@ -25,7 +25,6 @@ class MainWindowViewController:
         self.problems_view = self.view.problem_list_pane.tvwfrm_problems
         self.solution_view = self.view.lbl_solution
         self.timer_view = self.view.main_timer_view
-        return
 
     def apply_skins(self, settings: skins.SkinSettings) -> None:
         piece_skin, board_skin, komadai_skin = settings.get()
@@ -33,87 +32,68 @@ class MainWindowViewController:
         self.board_canvas.apply_board_skin(board_skin)
         self.board_canvas.apply_komadai_skin(komadai_skin)
         self.board_canvas.draw()
-        return
 
     def set_main_board(self, pos: Position) -> None:
         self.board_canvas.set_position(pos)
 
     def flip_main_board(self, want_upside_down: bool) -> None:
         self.board_canvas.flip_board(want_upside_down)
-        return
 
     def refresh_move_list(self) -> None:
         self.view.movelist_frame.refresh_content()
-        return
 
     def disable_problem_list_input(self) -> None:
         self.problems_view.disable_input()
-        return
 
     def enable_problem_list_input(self) -> None:
         self.problems_view.enable_input()
-        return
 
     def disable_move_input(self) -> None:
         move_input_handler = self.board_canvas.move_input_handler
         if move_input_handler is not None:
             move_input_handler.disable()
-        return
 
     def enable_move_input(self) -> None:
         move_input_handler = self.board_canvas.move_input_handler
         if move_input_handler is not None:
             move_input_handler.enable()
-        return
 
     def disable_move_navigation(self) -> None:
         self.view.movelist_frame.disable_display()
         self.view.board_frame.disable_buttons()
-        return
 
     def enable_move_navigation(self) -> None:
         self.view.movelist_frame.enable_display()
         self.view.board_frame.enable_buttons()
-        return
 
     def hide_solution(self) -> None:
         self.solution_view.hide_solution()
-        return
 
     def show_solution(self) -> None:
         self.solution_view.show_solution()
-        return
 
     def toggle_solution(self, _event: Optional[tk.Event] = None) -> None:
         self.solution_view.toggle_solution()
-        return
 
     def set_solution(self, solution_text: str) -> None:
         self.solution_view.set_solution_text(solution_text)
-        return
 
     def allow_only_pause_timer(self) -> None:
         self.timer_view.allow_only_pause()
-        return
 
     def allow_all_timer(self) -> None:
         self.timer_view.allow_all()
-        return
 
     def set_btns_allow_abort_speedrun(self) -> None:
         self.view.speedrun_frame.allow_abort_speedrun()
-        return
 
     def set_btns_allow_start_speedrun(self) -> None:
         self.view.speedrun_frame.allow_start_speedrun()
-        return
 
     def set_normal_nav_pane(self) -> None:
         self.view.update_nav_control_pane()
-        return
 
     def set_nav_pane(
         self, nav_pane_constructor: Callable[[tk.Widget], ttk.Frame]
     ) -> None:
         self.view.update_nav_control_pane(nav_pane_constructor)
-        return
